12e7a08d7feff726de015fa53d7798756302f823
Cauane Andrade <cauane.emanuela@hotmail.com>
2022-11-16 20:46:36 -0300

    feat: Add challenge resolution

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
-# Ruby
+# Customer Success Balacing Challenge
+Este desafio consiste em um sistema de balanceamento entre clientes e Customer Success (CSs). Os CSs são os Gerentes de Sucesso, são responsáveis pelo acompanhamento estratégico dos clientes.
 
-## Como rodar os testes
+Dependendo do tamanho do cliente - aqui nos referimos ao tamanho da empresa - nós temos que colocar CSs mais experientes para atendê-los.
+
+Um CS pode atender mais de um cliente e além disso os CSs também podem sair de férias, tirar folga, ou mesmo ficarem doentes. É preciso levar esses critérios em conta na hora de rodar a distribuição.
+
+Dado este cenário, o sistema distribui os clientes com os CSs de capacidade de atendimento mais próxima (maior) ao tamanho do cliente.
+
+
+# Ambiente
+- Versão executada: ruby 2.7.0p0
+- Ambiente: x86_64-linux-gnu
+
+
+## Executar os testes localmente
 
 No terminal, execute os comandos:
 
 ```
-cd ruby
+cd src/
 ruby customer_success_balancing.rb
 ```
+
+## Análise do algoritmo
+Utilizando-se da análise assintótica, a complexidade do algoritmo é `N Log N` (sub-linear).
+Para a memória, pode ser considerado O(n) - linear. Para isso, não é considerado o tamanho da entrada; mas sim atribuições e operações booleanas.
+
+### Resultados locais
+![Execução do testes locais](img/tests-execution-result.png)
+
+---
+
+## Mais informações
+Algoritmo desenvolvido por [Cauane Andrade](https://www.linkedin.com/in/cauaneandrade/).
+
+* [github.com/CauaneAndrade](https://github.com/CauaneAndrade/)
+* [linkedin.com/in/CauaneAndrade](https://www.linkedin.com/in/CauaneAndrade/)
--- src/customer_success_balancing.rb
+++ src/customer_success_balancing.rb
@@ -8,9 +8,58 @@ class CustomerSuccessBalancing
     @away_customer_success = away_customer_success
   end
 
-  # Returns the ID of the customer success with most customers
+  def self.get_customer_success_id (customer_success, customers)
+    """
+    Get the Id of the Customer Success with most Customers or 0 if repeated
+
+    :return: Int: the Customer Sucess Id
+    """
+    hash = {}
+    customer_success.each { |c| hash[c[:id]] = 0 }
+    customers.group_by { |c| c[:score] }.each {
+      |score, items|
+      cs = customer_success.select { |cs| cs[:score] >= score }.shift
+      if cs && !cs.empty?
+        hash[cs[:id]] += items.length
+      end
+    }
+
+    length = hash.select {|_, v| v == hash.values.max }.length
+    is_repeated = length != 1
+    return 0 if is_repeated
+    return hash.max_by{|k,v| v}[0]
+  end
+
+  def self.customer_success_filtered (customer_success_all, away_customer_success)
+    """
+    Filter available Customer Success.
+    Remove the Customer Success that aren`t available.
+
+    :return: The Customer Success array sorted 
+    """
+    customer_success = customer_success_all.select {
+      |cs| !away_customer_success.include? cs[:id]
+    }
+    return customer_success.sort_by { |hsh| hsh[:score] }
+  end
+
   def execute
-    # Write your solution here
+    """
+    Análise do algoritmo
+      Tempo de execução é no pior caso Sub-linear (n log n)
+
+    :return: the ID of the customer success with most customers
+    """
+    customers_arr = @customers.sort_by { |hsh| hsh[:score] }
+    customer_success_arr = self.class.customer_success_filtered @customer_success, @away_customer_success
+    
+    if !customers_arr.empty?
+      customer_success_arr = customer_success_arr.select{|k| k[:score] >= customers_arr[0][:score]}  # somente score maior ou igual ao mínimo score do Customer
+    end
+    if !customer_success_arr.empty?
+      customers_arr = customers_arr.select{|k| k[:score] <= customer_success_arr[-1][:score]}  # somente score menor ou igual ao máximo score do Customer Sucess
+    end
+    return self.class.get_customer_success_id customer_success_arr, customers_arr
   end
 end
 
